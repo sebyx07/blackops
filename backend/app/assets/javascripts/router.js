a876a181580a11f2ac0fcbb653eace16abd256c8
sebi <gore.sebyx@yahoo.com>
2017-07-20 17:49:50 +0300

    add rails app

---
+++ backend/app/assets/javascripts/router.js
@@ -0,0 +1,5 @@
+// For more information see: http://emberjs.com/guides/routing/
+
+Backend.Router.map(function() {
+  // this.resource('posts');
+});
---
+++ backend/config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  root 'ember#index'
+end
